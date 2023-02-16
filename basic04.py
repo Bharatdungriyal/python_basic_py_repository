6b1d98d039d2e94ad9c42084693b56b5ef730514
Bharatdungriyal <125379232+Bharatdungriyal@users.noreply.github.com>
2023-02-16 18:53:36 +0530

    Add files via upload

---
+++ basic04.py
@@ -0,0 +1,22 @@
+# making calculator
+
+X= input("What is X:")
+Y= input("what is Y:")
+A=int(X) + int(Y)
+print(f" Here your result:-\nthe addition of X and Y is {A}.")
+S=int(X)-int(Y)
+print(f"the subtraciton of X and Y is {S}.")
+M=int(X)*int(Y)
+print(f"the multiplication of X and Y is {M}.")
+D=int(X)/int(Y)
+print(f"the division of X and Y is {D}.")
+SQUARE=int(X)**2
+print(f"the square of X is {SQUARE}.")
+CUBE=int(X)**3
+print(f"the CUBE of X is {CUBE}.")
+Sq_root=int(X)**1/2
+print(f"the square root of X is {Sq_root}.")
+CUBE_root=int(X)**0.3333333333
+print(f"the cube root of X is {CUBE_root}.\n thanks for using calculator.")
+
+
